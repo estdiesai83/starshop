9935557f8f1436efeb243047ac795ffdd50a0608
estdiesai83 <estdiesai83@gmail.com>
2026-04-03 12:25:12 +0200

    añadido plantillas twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,14 +4,34 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 457;
+
+        // return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        // return $this->render('main/homepage.html.twig');
+        // return $this->render('main/homepage.html.twig', [
+        //     'numberOfStarships' => $starshipCount,
+        // ]);
+
+        // Pasando una matriz asociativa
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
